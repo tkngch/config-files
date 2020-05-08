ae3f97b5168df798b76eafe1e1aeefb0fbd450e0
Takao Noguchi <tkngch@runbox.com>
2020-05-08 14:13:31 +0100

    add xdg-utils configurations

--- install.sh
+++ install.sh
@@ -1,20 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
 here=$PWD
 
 for file in ".clang-format" ".gitconfig" ".mailcap" ".muttrc" ".Rprofile" ".sqliterc" ".tmux.conf" ".vimrc" ".Xresources" ".xinitrc" ".zshrc";
 do
-    if [[ $(readlink -f $HOME/$file) != $(readlink -f $here/$file) ]]; then
-        ln -i -s -T $here/$file $HOME/$file
+    if [[ $(readlink -f "$HOME/$file") != $(readlink -f "$here/$file") ]]; then
+        ln -i -s -T "$here/$file" "$HOME/$file"
         echo "linked $file"
     fi
 done
 
 for file in "user-dirs.dirs";
 do
-    ln -i -s -T $here/$file $XDG_CONFIG_HOME/$file
+    ln -i -s -T "$here/$file" "$XDG_CONFIG_HOME/$file"
     echo "linked $file"
 done
 
-mkdir -p $XDG_CONFIG_HOME/nvim
-ln -i -s -T $here/init.vim $XDG_CONFIG_HOME/nvim/init.vim
+ln -i -s -T "$here/mimeapps.list" "$HOME/.config/mimeapps.list"
+ln -i -s -T "$here/null.desktop" "$HOME/.local/share/applications/null.desktop"
+
+mkdir -p "$XDG_CONFIG_HOME/nvim"
+ln -i -s -T "$here/init.vim" "$XDG_CONFIG_HOME/nvim/init.vim"
