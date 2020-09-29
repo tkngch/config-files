82633a23da108a04a31526a9df8674d7ccc9addd
Takao Noguchi <tkngch@runbox.com>
2020-09-29 18:27:14 +0100

    update

--- home/emacs.d/init.el
+++ home/emacs.d/init.el
@@ -254,25 +254,28 @@
 ;;           (setq neo-theme 'arrow)
 ;;           (setq neo-default-system-application nil)))
 
-(use-package eglot
-  :ensure t
-  :init (progn
-          (add-hook 'scala-mode-hook 'eglot-ensure)))
-
-;; (use-package lsp-mode
+;; (use-package eglot
 ;;   :ensure t
 ;;   :init (progn
-;;           (setq-default lsp-keymap-prefix "s-l")
-;;           (setq-default lsp-completion-provider :capf)
-;;           ;; Performance tuning for lsp-mode
-;;           (setq gc-cons-threshold 100000000)
-;;           (setq read-process-output-max (* 1024 1024)) ;; 1mb
-;;           )
-;;   :hook (
-;;          (python-mode . lsp)
-;;          (scala-mode . lsp)
-;;          )
-;;   :commands lsp)
+;;           (add-hook 'scala-mode-hook 'eglot-ensure)))
+
+(use-package lsp-mode
+  :ensure t
+  :init (progn
+          (setq-default lsp-keymap-prefix "s-l")
+          (setq-default lsp-completion-provider :capf)
+          ;; Performance tuning for lsp-mode
+          (setq gc-cons-threshold 100000000)
+          (setq read-process-output-max (* 1024 1024)) ;; 1mb
+          )
+  :hook (
+         (python-mode . lsp)
+         (scala-mode . lsp)
+         )
+  :commands lsp)
+
+(use-package lsp-metals
+  :ensure t)
 
 ;; Use code-linter.
 (use-package flycheck
@@ -419,7 +422,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(beacon all-the-icons dired-sidebar ivy lua-mode pyvenv magit vue-mode use-package scala-mode format-all flycheck evil)))
+   '(lsp-metals beacon all-the-icons dired-sidebar ivy lua-mode pyvenv magit vue-mode use-package scala-mode format-all flycheck evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
