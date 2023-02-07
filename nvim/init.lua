23bed089cb01118cae3c5133fe02e3594875efe9
TAKAO NOGUCHI <takao@takaos-air.home>
2023-02-07 22:09:42 +0000

    revise

--- nvim/init.lua
+++ nvim/init.lua
@@ -135,7 +135,10 @@ function configure_telescope()
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+    -- list available help tags to show the help info
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    -- list previously open files
+    vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {})
 end
 
 function configure_carbon()
