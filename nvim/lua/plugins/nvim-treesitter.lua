42499ed3e295a34b2d4594c21977a24e96703c41
Takao Noguchi <tkngch@runbox.com>
2024-02-22 22:40:20 +0000

    configure incremental_selection

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -10,6 +10,15 @@ return { -- syntax highlighting and more
                 enable = true,
                 additional_vim_regex_highlighting = false,
             },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "gnn", -- set to `false` to disable one of the mappings
+                    node_incremental = "grn",
+                    scope_incremental = "grc",
+                    node_decremental = "grm",
+                },
+            },
             indent = {
                 enable = true,
             },
