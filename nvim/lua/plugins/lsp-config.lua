bcda62fe486e1b04005b64b4ed61e2120c1fce31
Takao Noguchi <tkngch@runbox.com>
2024-02-28 19:18:40 +0000

    add lsp-config

---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,80 @@
+local lua_ls_setup = {
+    -- Taken from nvim-lspconfig: https://github.com/neovim/nvim-lspconfig/blob/0ebcaedb2feb946e56658e0c9b6155fb6c8ad62b/lua/lspconfig/server_configurations/lua_ls.lua#L46
+    on_init = function(client)
+        local path = client.workspace_folders[1].name
+        if not vim.loop.fs_stat(path .. "/.luarc.json") and not vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+            client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
+                Lua = {
+                    runtime = {
+                        -- Tell the language server which version of Lua you're using
+                        -- (most likely LuaJIT in the case of Neovim)
+                        version = "LuaJIT",
+                    },
+                    -- Make the server aware of Neovim runtime files
+                    workspace = {
+                        checkThirdParty = false,
+                        library = {
+                            vim.env.VIMRUNTIME,
+                            -- Depending on the usage, you might want to add additional paths here.
+                            -- E.g.: For using `vim.*` functions, add vim.env.VIMRUNTIME/lua.
+                            -- "${3rd}/luv/library"
+                            -- "${3rd}/busted/library",
+                        },
+                        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+                        -- library = vim.api.nvim_get_runtime_file("", true)
+                    },
+                },
+            })
+        end
+        return true
+    end,
+}
+
+return {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+    config = function()
+        -- Setup language servers.
+        local lspconfig = require("lspconfig")
+        lspconfig.pyright.setup({})
+        require("lspconfig").lua_ls.setup(lua_ls_setup)
+
+        -- Global mappings.
+        -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+        vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+        vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+        vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+
+        -- Use LspAttach autocommand to only map the following keys
+        -- after the language server attaches to the current buffer
+        vim.api.nvim_create_autocmd("LspAttach", {
+            group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+            callback = function(ev)
+                -- Enable completion triggered by <c-x><c-o>
+                vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+                -- Buffer local mappings.
+                -- See `:help vim.lsp.*` for documentation on any of the below functions
+                local opts = { buffer = ev.buf }
+                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+                vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+                vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+                vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+                vim.keymap.set("n", "<space>wl", function()
+                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                end, opts)
+                vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+                vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+                vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+                vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+                vim.keymap.set("n", "<space>f", function()
+                    vim.lsp.buf.format({ async = true })
+                end, opts)
+            end,
+        })
+    end,
+}
