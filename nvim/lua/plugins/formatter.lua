c1aa5e03e8cb65dd02d5489cf752c5582bd850e7
Takao Noguchi <tkngch@runbox.com>
2024-05-19 15:08:46 +0100

    Add json and python

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -3,8 +3,10 @@ return { -- autoformat code
     config = function()
         require("formatter").setup({
             filetype = {
+                json = require("formatter.filetypes.json").prettier,
                 lua = require("formatter.filetypes.lua").stylua,
                 markdown = require("formatter.filetypes.markdown").prettier,
+                python = require("formatter.filetypes.python").ruff,
                 ["*"] = require("formatter.filetypes.any").remove_trailing_whitespace,
             },
         })
