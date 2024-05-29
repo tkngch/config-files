888acd0fae2565c30ce2b8aeed23a7a7c8c245ce
Takao Noguchi <tkngch@runbox.com>
2024-05-29 22:32:30 +0100

    nvim v0.10

--- nvim/init.lua
+++ nvim/init.lua
@@ -67,7 +67,7 @@ end
 
 -- Manage packages with lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
     vim.fn.system({
         "git",
         "clone",
