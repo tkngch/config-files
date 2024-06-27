98f6e921adca73f5f0a13b9214260a550db399a5
Takao Noguchi <tkngch@runbox.com>
2024-06-27 22:10:39 +0100

    add jdtls

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -4,6 +4,7 @@ return {
     config = function()
         local lspconfig = require("lspconfig")
         lspconfig.gopls.setup({})
+        lspconfig.jdtls.setup({})
         lspconfig.lua_ls.setup({})
         lspconfig.pyright.setup({})
         lspconfig.rust_analyzer.setup({})
