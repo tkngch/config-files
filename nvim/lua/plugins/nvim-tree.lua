7ab1d2e5a3c928edc1932fd932bf5add6a8b6f5b
Takao Noguchi <tkngch@runbox.com>
2025-09-29 17:34:13 +0100

    Drop icons from nvim-tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,16 +1,9 @@
 return { -- file explorer
     "nvim-tree/nvim-tree.lua",
-    dependencies = {
-        "nvim-tree/nvim-web-devicons", -- optional
-        -- nvim-web-devicons requires a patched font.
-        -- To install on Mac, `brew tap homebrew/cask-fonts` and then `brew install --cask font-hack-nerd-font`
-    },
     config = function()
         -- disable netrw
         vim.g.loaded_netrw = 1
         vim.g.loaded_netrwPlugin = 1
-
-        local api = require("nvim-tree.api")
 
         require("nvim-tree").setup({
             -- Changes the tree root directory on `DirChanged` and refreshes the tree.
@@ -24,9 +17,23 @@ return { -- file explorer
             },
             renderer = {
                 group_empty = true,
+                icons = {
+                    git_placement = "after",
+                    show = {
+                        file = false,
+                        folder = false,
+                        folder_arrow = false,
+                        git = true,
+                        modified = true,
+                        hidden = false,
+                        diagnostics = false,
+                        bookmarks = false,
+                    },
+                },
             },
         })
 
+        local api = require("nvim-tree.api")
         vim.keymap.set("", "<leader>tt", function()
             if api.tree.is_visible() then
                 api.tree.collapse_all()
